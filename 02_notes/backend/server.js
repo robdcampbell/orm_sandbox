879713b8e49150a1ea4451588b14f640deaeaa43
robdcampbell <rdeancampbell@gmail.com>
2021-04-23 08:49:46 -0400

    Implement Express Router with Notes projects

--- 02_notes/backend/server.js
+++ 02_notes/backend/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// simple test route
 app.get("/", (req, res) => {
-  res.send("Pterrible Pterodactyls Batman!");
+  res.send("Pterrible Pterodactyls!");
 });
 
-// app.use("/profile", "./routes/notes.js");
+app.use("/profile", require("./routes/notes"));
 
 app.listen(PORT, (req, res) => {
   console.log(`Server launched on port: ${PORT}`);
