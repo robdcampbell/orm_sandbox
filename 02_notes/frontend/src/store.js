5a962c8f352c33326d4e7652187482b3f8e1b631
robdcampbell <rdeancampbell@gmail.com>
2021-04-30 22:23:30 -0400

    Continue configuring store.js

--- 02_notes/frontend/src/store.js
+++ 02_notes/frontend/src/store.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
+const reducer = combineReducers([]);
+
 const middleware = [thunk];
 
 const initialState = {
@@ -9,4 +11,10 @@ const initialState = {
   email: "bob@gmail.com",
 };
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+// LOOK AT PRODUCT LIAST REDUCER
