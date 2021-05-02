241aa9afc55c8a6330cbf792a1746a25f572ef06
robdcampbell <rdeancampbell@gmail.com>
2021-05-02 09:17:03 -0400

    Notes Redux structure set up - begin adding full CRUD functionality

--- 02_notes/frontend/src/actions/noteActions.js
+++ 02_notes/frontend/src/actions/noteActions.js
@@ -4,3 +4,25 @@ import {
   NOTE_LIST_FAIL,
 } from "../constants/noteConstants";
 import axios from "axios";
+
+export const listNotes = (keyword = "", pageNumber = "") => async (
+  dispatch
+) => {
+  try {
+    dispatch({ type: NOTE_LIST_REQUEST });
+    const { data } = await axios.get();
+
+    dispatch({
+      type: NOTE_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: NOTE_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- 02_notes/frontend/src/store.js
+++ 02_notes/frontend/src/store.js
@@ -1,14 +1,25 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import {
+  noteListReducer,
+  noteAddReducer,
+  noteUpdateReducer,
+  noteDeleteReducer,
+} from "./reducers/notesReducers";
 
-const reducer = combineReducers([]);
+const reducer = combineReducers({
+  noteList: noteListReducer,
+  // noteAdd: noteAddReducer,
+  // noteUpdate: noteUpdateReducer,
+  // noteDelete: noteDeleteReducer,
+});
 
 const middleware = [thunk];
 
 const initialState = {
-  name: "Bob",
-  email: "bob@gmail.com",
+  // name: "Bob",
+  // email: "bob@gmail.com",
 };
 
 const store = createStore(
