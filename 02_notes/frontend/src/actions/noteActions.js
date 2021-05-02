5f5d4b324432d53856a1ad8e0664996379378815
robdcampbell <rdeancampbell@gmail.com>
2021-05-02 09:01:40 -0400

    Note constants and reducers structure done

---
+++ 02_notes/frontend/src/actions/noteActions.js
@@ -0,0 +1,6 @@
+import {
+  NOTE_LIST_REQUEST,
+  NOTE_LIST_SUCCESS,
+  NOTE_LIST_FAIL,
+} from "../constants/noteConstants";
+import axios from "axios";
--- 02_notes/frontend/src/reducers/notesReducers.js
+++ 02_notes/frontend/src/reducers/notesReducers.js
@@ -1,7 +1,24 @@
-export const noteListReducer = (state = { notes: [] }, action) => {};
+import {
+  NOTE_LIST_REQUEST,
+  NOTE_LIST_SUCCESS,
+  NOTE_LIST_FAIL,
+} from "../constants/noteConstants";
 
-export const noteAddReducer = (state = { notes: [] }, action) => {};
+export const noteListReducer = (state = { notes: [] }, action) => {
+  switch (action.type) {
+    case NOTE_LIST_REQUEST:
+      return { loading: true, notes: [] };
+    case NOTE_LIST_SUCCESS:
+      return { loading: true, notes: action.payload.notes };
+    case NOTE_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
-export const noteUpdateReducer = (state = { notes: [] }, action) => {};
+export const noteAddReducer = (state = {}, action) => {};
 
-export const noteDeleteReducer = (state = { notes: [] }, action) => {};
+export const noteUpdateReducer = (state = {}, action) => {};
+
+export const noteDeleteReducer = (state = {}, action) => {};
